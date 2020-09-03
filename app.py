6d56f3ff3612395587c849af3d717ab8a5a8ea2f
biermaja <biermaja@umich.edu>
2020-09-02 21:50:49 -0400

    took away all globals, pass all data thru urls now

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, url_for
 # from flask_sqlalchemy import SQLAlchemy
 import datetime
+import time
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import spotipy.util as util
@@ -50,15 +51,9 @@ def authorize_lastfm():
 
 #YOUTUBE API STUFF
 def authorize_youtube():
-    api_key = 'AIzaSyCDiIJ_wOXgXDIz0ZHj_FHQMpHJBH9i4bE'
+    api_key = 'AIzaSyBhj5wg0cm1sETpB0sGyDN3sEWacnzZfRM'
     yt = YouTubeDataAPI(api_key)
     return yt
-    # print(yt.verify_key())
-    # search = yt.search(q='Saint Pablo Kanye West')
-    # video_id = search[0]['video_id']
-    # metadata = yt.get_video_metadata(video_id)
-    # view_count = metadata['video_view_count']
-    # print(f"Saint Pable has {view_count} views on youtube")
 
 
 
@@ -84,27 +79,20 @@ class Playlist:
         self.name = name
         self.songs = []
 
-    def populate(self):
-        # results = sp.user_playlist_tracks(playlist_id = self.id)
-        # tracks = results['items']
-        tracks = get_all_playlist_tracks(playlist_id_in=self.id)
+    def populate(self, sp):
+        tracks = get_all_playlist_tracks(playlist_id_in=self.id, sp = sp)
         for song in tracks:
             temp_name = song['track']['name']
             temp_artist = song['track']['artists'][0]['name']
             temp_album = song['track']['album']['name']
-            # temp_play_count = network.get_track(temp_artist, temp_name).get_playcount()
             temp_play_count = 0
             temp_clip = song['track']['preview_url']
             self.songs.append(Song(temp_name, temp_artist, temp_album, temp_play_count, temp_clip))
- 
-
-        # randomize list of songs
-        random.shuffle(self.songs)
         
     def toJson(self):
         return json.dumps(self, default=lambda o: o.__dict__)
 
-def get_all_playlist_tracks(playlist_id_in):
+def get_all_playlist_tracks(playlist_id_in, sp):
     results = sp.user_playlist_tracks(playlist_id = playlist_id_in)
     tracks = results['items']
     while results['next']:
@@ -117,8 +105,6 @@ def create_playlists(sp):
     ret_pls = []
     for playlist in lists:
         ret_pls.append(Playlist(playlist['name'], playlist['id']))
-    # for pl in ret_pls:
-    #     print(f"Name: {pl.name} id: {pl.id}")
     return ret_pls
 
 def playlist_to_id(pls, id):
@@ -127,21 +113,14 @@ def playlist_to_id(pls, id):
             return pl
     return None
 
+def create_playlist(sp, id):
+    pl = sp.playlist(id)
+    return Playlist(pl['name'], pl['id'])
 
-#YOutube API Testing
-authorize_youtube()
-
-#initializing vars
-username = None
-playlists = None
-chosen_playlist = None
-sp = None
-auth_manager = None
-song_counter = 0
-score = -1
 client_id = '379b15e111a14089ae41a384d0db80a2'
 client_secret = 'f487fb0030f640eabf35f5ceefffe427'
-redirect_uri = 'https://playlistbattle.herokuapp.com'
+# redirect_uri = 'https://playlistbattle.herokuapp.com'
+redirect_uri = 'http://localhost:8000'
 scope = "user-library-read playlist-read-private playlist-read-collaborative"
 
 #CREATE SERVER
@@ -163,12 +142,11 @@ def session_cache_path():
 #THE ACTUAL SERVER PART
 @app.route('/')
 def index():
-    global sp
     if not session.get('uuid'):
         # Step 1. Visitor is unknown, give random ID
         session['uuid'] = str(uuid.uuid4())
 
-    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private',
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-library-read playlist-read-private playlist-read-collaborative',
                                                 client_id = client_id,
                                                 client_secret = client_secret,
                                                 redirect_uri = redirect_uri,
@@ -187,55 +165,44 @@ def index():
     
     # Step 4. Signed in, display data
     sp = spotipy.Spotify(auth_manager=auth_manager)
-    return redirect('/choose')
+    playlists = create_playlists(sp)
+    access_token = auth_manager.get_access_token()['access_token']
+    return redirect(url_for('choose', access_token = access_token))
+    # return render_template('choose.html', playlists = playlists, name = sp.me()['display_name'])
 
 
     
 
-@app.route('/choose', methods = ['GET', 'POST'])
-def choose(): 
-    global score, song_counter, chosen_playlist, sp, playlists, auth_manager, yt_api
-    global song_counter
-    global chosen_playlist  
-    global sp
-    global network
+@app.route('/choose/<access_token>', methods = ['GET', 'POST'])
+def choose(access_token): 
+    sp = spotipy.Spotify(auth = access_token)
     score = -1
-    song_counter = 0
-    chosen_playlist = None
-    network = authorize_lastfm()
-    yt_api = authorize_youtube()
     playlists = create_playlists(sp)
-    return render_template('choose.html', playlists = playlists, name = sp.me()['display_name'])
+    seed = int(round(time.time() * 1000))
+    print(f"Your seed is {seed}")
+    return render_template('choose.html', playlists = playlists, name = sp.me()['display_name'], access_token = access_token, seed = seed)
 
-@app.route('/game', methods = ['GET', 'POST'])
-def game():
-    # if(request.method == 'POST'):
-    #     return "hi"
-    # else:
-    global chosen_playlist, song_counter, score, playlists, network, yt_api    
-    global song_counter
-    global score
-    global playlists
+@app.route('/game/<access_token>/<pl_id>/<int:seed>/<int:song_counter>/<int:score>/<int:pl_made>', methods = ['GET', 'POST'])
+def game(access_token, pl_id, song_counter, score, pl_made, seed):
+    yt_api = authorize_youtube()
+    sp = spotipy.Spotify(auth = access_token)
+    if pl_id == 'none':
+        pl_id = request.form.get('action')
+    chosen_playlist = create_playlist(sp, pl_id)
+    chosen_playlist.populate(sp)
+    random.Random(seed).shuffle(chosen_playlist.songs)
+    # shuffle playlist the same way
+    song1 = chosen_playlist.songs[song_counter].update_play_count(yt_api)
+    song2 = chosen_playlist.songs[song_counter+1].update_play_count(yt_api)
+    # song_counter += 1
+    # score += 1
+    return render_template('game.html', playlist = chosen_playlist, song1 = song1, song2 = song2, pl_id = pl_id,
+        score = score, song_counter = song_counter, access_token = access_token, seed = seed)
 
-    if(chosen_playlist == None):
-        chosen_playlist_id = request.form['action'] # action form, the name of the playlist
-        chosen_playlist = playlist_to_id(playlists, chosen_playlist_id)
-        chosen_playlist.populate()
-    if(song_counter < len(chosen_playlist.songs)):
-        song1 = chosen_playlist.songs[song_counter].update_play_count(yt_api)
-        song2 = chosen_playlist.songs[song_counter+1].update_play_count(yt_api)
-        song_counter += 1
-        score += 1
-    return render_template('game.html', playlist = chosen_playlist, song1 = song1, song2 = song2, score = score)
-
-@app.route('/restart', methods = ['GET', 'POST'])
-def restart():
-    global song_counter
-    global score
-    song_counter = 0
-    score = -1
-    random.shuffle(chosen_playlist.songs)
-    return redirect("/game")
+@app.route('/restart/<access_token>/<pl_id>', methods = ['GET', 'POST'])
+def restart(access_token, pl_id):
+    seed = int(round(time.time() * 1000))
+    return redirect(f"/game/{access_token}/{pl_id}/{seed}/0/0/1")
 
 @app.route('/sign_out')
 def sign_out():
@@ -249,4 +216,4 @@ def sign_out():
     return redirect('/')
 
 if __name__ == "__main__":
-    app.run(debug = True, port = 8080)
+    app.run(debug = True, port = 8000)
